6c6a29a412e9931dabb90334cbca806a5db88b6a
MrDhillon <harman.singh.dhillon@gmail.com>
2015-08-31 16:20:42 -0400

    added prompt

--- contact_list.rb
+++ contact_list.rb
@@ -3,3 +3,21 @@ require_relative 'contact_database'
 
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+
+
+def operation
+
+  puts "Please input a command or type help for menu"
+  input = gets.chomp.downcase
+
+  if input == "help"
+    puts "Here is a list of commands:
+              new - Create a new contact
+              list - List all contacts
+              show - Show a contact
+              find - Find a contact"
+  end
+
+end
+
+operation

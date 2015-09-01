b4421dc199719c0625559bec6282fc16812f7f82
MrDhillon <harman.singh.dhillon@gmail.com>
2015-09-01 12:34:54 -0400

    show and list added

--- contact.rb
+++ contact.rb
@@ -1,33 +1,50 @@
 class Contact
- 
+
   attr_accessor :name, :email
 
   def initialize(name, email)
     # TODO: assign local variables to instance variables
+    @name = name
+    @email = email
+
   end
- 
+
   def to_s
     # TODO: return string representation of Contact
+
   end
- 
+
   ## Class Methods
   class << self
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
+     # contact = ContactDatabase.create(@name,@email)
+     Contact.new(@name,@password)
     end
- 
+
     def find(term)
       # TODO: Will find and return contacts that contain the term in the first name, last name or email
+
     end
- 
-    def all
+
+    def all(list)
       # TODO: Return the list of contacts, as is
+      list.each do |row|
+        puts "User id: #{row[0]} Username:#{row[1]} Email: #{row[2]}"
+      end
+
     end
-    
-    def show(id)
+
+    def show(list,id)
       # TODO: Show a contact, based on ID
+      shown_location = list.select  {|row| row[0] == id}
+      if shown_location != nil
+          shown_location.each do |row|
+        puts "User id: #{row[0]} Username:#{row[1]} Email: #{row[2]}"
+      end
+      else
+        puts "No user found"
+      end
     end
-    
   end
- 
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'contact'
 require_relative 'contact_database'
 
@@ -8,9 +9,11 @@ require_relative 'contact_database'
 def operation
 
   puts "Please input a command or type help for menu"
-  input = gets.chomp.downcase
+  input = gets.chomp.downcase.split(" ")
+  string = input[0]
+  number = input[1]
 
-  if input == "help"
+  if string == "help"
     puts "Here is a list of commands:
               new - Create a new contact
               list - List all contacts
@@ -18,6 +21,32 @@ def operation
               find - Find a contact"
   end
 
+  if string == "new"
+    puts "What is the contact name?"
+    name_input = gets.chomp
+    puts "what is the contact email?"
+    email_input = gets.chomp
+    puts "Would you like to add this to your contacts name:#{name_input} email:#{email_input}?"
+    yes_no = gets.chomp.downcase
+
+      if yes_no == "yes"
+        contact = Contact.create(name_input,email_input)
+        ContactDatabase.create(name_input,email_input)
+      else
+        puts "The information was not added"
+      end
+  elsif string =="list"
+         list = ContactDatabase.all
+         Contact.all(list)
+        # puts contacts
+        # contacts.each { |line| puts line.to_s }
+  elsif string == "show"
+        show_id = ContactDatabase.show
+        Contact.show(show_id,number)
+  end
+
 end
 
+
 operation
+
